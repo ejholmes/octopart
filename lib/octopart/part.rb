0acc1f778becffb67cb86c5cf1551a59fa97b5b2
Eric J. Holmes <eric@ejholmes.net>
2012-04-26 01:37:03 -0700

    Spec for search.

--- lib/octopart/part.rb
+++ lib/octopart/part.rb
@@ -8,8 +8,14 @@ module Octopart
         self.build(response)
       end
 
-      def search(query, options = nil)
-        
+      def search(query, options = {})
+        params = options.merge(q: query)
+        response = JSON.parse(self.get('parts/search', params))
+        parts = []
+        response['results'].each do |part|
+          parts << part['item']
+        end
+        self.build(parts)
       end
 
       def match(manufacturer, mpn)
@@ -20,10 +26,24 @@ module Octopart
         
       end
 
-      def build(hash)
-        hash = Hashie::Mash.new(hash)
+      def build(object)
+        if object.is_a?(Array)
+          parts = []
+          object.each do |obj|
+            parts << self.build_single(obj)
+          end
+          parts
+        elsif object.is_a?(Hash)
+          self.build_single(object)
+        else
+          raise "What is this? I don't even..."
+        end
+      end
+
+      def build_single(object)
+        object = Hashie::Mash.new(object)
         part = self.new
-        part.replace(hash)
+        part.replace(object)
         part
       end
 
--- spec/lib/part_spec.rb
+++ spec/lib/part_spec.rb
@@ -12,7 +12,16 @@ describe Octopart::Part do
   end
 
   describe "#search" do
-    
+    context "when given a query" do
+      use_vcr_cassette
+      subject { described_class.search('resistor') }
+
+      it { should be_a(Array) }
+
+      it "each object in the array should be a part" do
+        subject.each { |part| part.should be_a(Octopart::Part) }
+      end
+    end
   end
 
   describe "#match" do
